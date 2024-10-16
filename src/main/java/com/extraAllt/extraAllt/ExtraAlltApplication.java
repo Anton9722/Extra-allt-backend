7200b9e1e9304bb9882288ae0d89fa462c671855
Anton Johansson <anton9797johansson@hotmail.com>
2024-10-16 12:18:08 +0200

    first commit

---
+++ src/main/java/com/extraAllt/extraAllt/ExtraAlltApplication.java
@@ -0,0 +1,13 @@
+package com.extraAllt.extraAllt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtraAlltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtraAlltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/extraAllt/extraAllt/ExtraAlltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extraAllt.extraAllt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtraAlltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
